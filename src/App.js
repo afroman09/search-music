aa54c84e1e2e94621a35abc624946faaba5f44f4
afroman09 <shinp09@gmail.com>
2021-01-30 17:34:41 +0900

    Dropboxの実装

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
-// import SearchInput from "./components/Atoms/SearchInput";
-// import StartButton from "./components/Atoms/StartButton";
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <h1>好みの曲を知ろう</h1>
-          <Route exact path="/Home" component={Home} />
-        </Switch>
-      </Router>
+          <h1>test</h1>
+          <Home />
     </div>
   );
 };
---
+++ src/components/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '947317710f7748f3b83fe1658ae35112',
+        ClientSecret: 'aeb789595a3e432c9d7ecc6d874b70fa'
+    }
+}
+
+export { Credentials };
