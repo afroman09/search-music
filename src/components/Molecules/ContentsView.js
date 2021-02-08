6393b3bbe36baeec6f502ce82e5336bf39481c5a
afroman09 <shinp09@gmail.com>
2021-02-08 17:39:33 +0900

    似ている曲のデータ情報を取得

--- src/components/Molecules/ContentsView.js
+++ src/components/Molecules/ContentsView.js
@@ -1,24 +1,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import ReactAudioPlayer from 'react-audio-player';
+import ReactAudioPlayer from "react-audio-player";
 import Style from "./ContentsView.module.scss";
 
 const ContentsView = (props) => {
   return (
-    <div className={Style.container}> 
+    <div className={Style.container}>
       <img className={Style.img} src={props.searchContents.trackImg} />
       <p className={Style.name}>{props.searchContents.artistName}</p>
       <p>{props.searchContents.trackName}</p>
-      <ReactAudioPlayer 
+      <ReactAudioPlayer
         className={Style.audio}
         src={props.searchContents.trackPreviewURL}
         controls
       />
       <div className={Style.wrapper}>
-          <p>{props.trackInformation.danceability}</p>
-          <p>{props.trackInformation.trackKey}</p>
-          <p>{props.trackInformation.mode}</p>
+        <p>{props.trackInformation.danceability}</p>
+        <p>{props.trackInformation.key}</p>
+        <p>{props.trackInformation.mode}</p>
       </div>
+      {/* <div className={Style.wrapper}>
+        <p>{props.similarContents.tracks}</p>
+      </div> */}
     </div>
   );
 };
--- src/components/Templates/Search.js
+++ src/components/Templates/Search.js
@@ -2,25 +2,29 @@ import React, { useState, useEffect } from "react";
 import Header from "../Molecules/Header";
 import { Credentials } from "../Credentials";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import ContentsView from "../Molecules/ContentsView";
-import ReturnBtn from "../Atoms/ReturnBtn";
 
 const Search = () => {
   const spotify = Credentials();
 
   const [token, setToken] = useState("");
-  const [trackInformation, setTracksInformation] = useState({
-    danceability: "",
-    mode: "",
-    trackKey: "",
-  });
+  const [trackInformation, setTracksInformation] = useState(
+  //   {
+  //   danceability: "",
+  //   trackKey: "",
+  //   mode: "",
+  // }
+  ''
+  );
   const [searchContents, setSearchContents] = useState({
     artistName: "",
+    artistId: "",
     trackPreviewURL: "",
     trackImg: "",
     trackName: "",
   });
+  const [similarContents, setSimilarContents] = useState("");
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
@@ -44,35 +48,16 @@ const Search = () => {
 
       /* アクセスTokenを発行 END */
 
-      /* 楽曲情報を取得 START */
-      // 付与されたtokenを使い、ジャンルにアクセス
-      // 取得したジャンルをgenreに適用
-      axios(`https://api.spotify.com/v1/audio-features?ids=${queryResult}`, {
-        method: "GET",
-        headers: { Authorization: "Bearer " + tokenResponse.data.access_token },
-      })
-        .then((tracksReaponse) => {
-          setTracksInformation({
-            danceability: tracksReaponse.data.audio_features[0].danceability,
-            mode: tracksReaponse.data.audio_features[0].mode,
-            trackKey: tracksReaponse.data.audio_features[0].key,
-          });
-        })
-        .catch((err) => {
-          console.log("err:", err);
-        });
-
-      /* 楽曲情報を取得 END */
-
       /* アーティスト情報を取得 START */
 
       axios(`https://api.spotify.com/v1/tracks/${queryResult}`, {
         method: "GET",
-        headers: { Authorization: "Bearer " + tokenResponse.data.access_token },
+        headers: { Authorization: "Bearer " + token },
       })
         .then((trackContentsReaponse) => {
           setSearchContents({
             artistName: trackContentsReaponse.data.artists[0].name,
+            artistId: trackContentsReaponse.data.artists[0].id,
             trackPreviewURL: trackContentsReaponse.data.preview_url,
             trackImg: trackContentsReaponse.data.album.images[0].url,
             trackName: trackContentsReaponse.data.name,
@@ -83,8 +68,66 @@ const Search = () => {
         });
 
       /* アーティスト情報を取得 END */
+
+      /* 楽曲情報を取得 START */
+      // 付与されたtokenを使い、ジャンルにアクセス
+      // 取得したジャンルをgenreに適用
+      axios(`https://api.spotify.com/v1/audio-features?ids=${queryResult}`, {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+      })
+        .then((tracksReaponse) => {
+          setTracksInformation(
+          //   {
+          //   danceability: tracksReaponse.data.audio_features[0].danceability,
+          //   trackKey: tracksReaponse.data.audio_features[0].key,
+          //   mode: tracksReaponse.data.audio_features[0].mode,
+          // }
+          tracksReaponse.data.audio_features[0]
+          );
+        })
+        .catch((err) => {
+          console.log("err:", err);
+        });
+
+      /* 楽曲情報を取得 END */
+
+      /* 似ている曲を取得 START */
+
+      axios(
+        `https://api.spotify.com/v1/recommendations?limit=3&market=US`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+          params: {
+            seed_tracks: queryResult,
+            target_danceability: trackInformation.danceability,
+            target_energy: trackInformation.energy,
+            target_key: trackInformation.key,
+            target_loudness: trackInformation.loudness,
+            target_mode: trackInformation.mode,
+            min_popularity: 0,
+            target_tempo: trackInformation.tempo,  
+            target_time_signature: trackInformation.signature,
+            target_valence: trackInformation.valence
+          }
+        }
+      )
+        .then((similarContentsReaponse) => {
+          setSimilarContents(similarContentsReaponse.data);
+        })
+        .catch((err) => {
+          console.log("err:", err);
+        });
+
+      /* 似ている曲を取得 END */
     });
-  }, [location.search, queryResult, spotify.ClientId, spotify.ClientSecret]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    queryResult,
+  ]);
 
   return (
     <div>
@@ -92,10 +135,18 @@ const Search = () => {
       <ContentsView
         searchContents={searchContents}
         trackInformation={trackInformation}
+        similarContents={similarContents}
       />
-      <ReturnBtn />
+      <Link to="/">
+        <button>TOP</button>
+      </Link>
     </div>
   );
 };
 
 export default Search;
+
+// クリックされたら値を取得する関数を定義し、そこにトークン以外のaxiosを記述
+// APIファイルを作成し、処理を一つにまとめる？
+// 呼び出しの際は関数で呼び出す
+// Header→contentsViewに結果を渡すのが正しい流れ？＾
