eb1e2aaeaab01f7566f25e758a186441576a57e7
afroman09 <shinp09@gmail.com>
2021-02-18 10:47:53 +0900

    README

--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -1,8 +1,8 @@
 const Credentials = () => {
 
     return {
-        ClientId: '947317710f7748f3b83fe1658ae35112',
-        ClientSecret: 'aeb789595a3e432c9d7ecc6d874b70fa'
+        ClientId: 'YOUR_SPOTIFY_CLIENT_ID',
+        ClientSecret: 'YOUR_SPOTIFY_CLIENT_SECRET'
     }
 }
 
